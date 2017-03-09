c579cefba9f3b22157125a9da2c451fb6c1c6e39
NoahFlowa <osterhoutn@gmail.com>
2017-03-08 21:49:45 -0500

    Edited routes.py

--- routes.py
+++ routes.py
@@ -5,8 +5,8 @@ from forms import SignupForm, LoginForm, AddressForm
 app = Flask(__name__)
 
 # add your Heroku Postgres database URL here:
-#app.config['SQLALCHEMY_DATABASE_URI'] = secure-springs-12593.herokuapp.com
-#db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = secure-springs-12593.herokuapp.com
+db.init_app(app)
 
 app.secret_key = "development-key"
 
